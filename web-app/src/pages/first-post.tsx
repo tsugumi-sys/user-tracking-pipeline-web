eaa03662b071ee3cc174d51e2a43f5d86ccc0bb0
tsugumi-sys <tidemark0105@gmail.com>
2024-03-19 21:57:55 +0900

    Add next files

---
+++ web-app/src/pages/first-post.tsx
@@ -0,0 +1,7 @@
+export default function FirstPost() {
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <h1>First Post</h1>
+    </div>
+  );
+}
